b51c724d5669a173bfb484138d33a945ad664b88
Xoneris <kely42@gmx.ch>
2025-04-03 05:12:44 +0200

    just some progress

--- src/components/content/Accordion.svelte
+++ src/components/content/Accordion.svelte
@@ -3,7 +3,7 @@
     let open = false
 </script>
 
-<div class="flex flex-col border rounded-lg bg-white">
+<div id="accordion" class="flex flex-col border rounded-lg bg-white">
     <button 
         class="{ open === true ? "border-b" : "delay-[400ms]" } p-2 rounded-lg w-full flex justify-between transition-all text-neutral-800 bg-neutral-300 cursor-pointer"
         on:click={() => open = !open}
--- src/content.config.ts
+++ src/content.config.ts
@@ -5,5 +5,14 @@ import { glob, file } from 'astro/loaders';
 const kompra1 = defineCollection({
   loader: glob({ pattern: ["**/*.mdx", "**/*.md"], base: "./src/content/kompra1"}),
 });
+const organisieren = defineCollection({
+  loader: glob({ pattern: ["**/*.mdx", "**/*.md"], base: "./src/content/organisieren"}),
+});
+const einbindung = defineCollection({
+  loader: glob({ pattern: ["**/*.mdx", "**/*.md"], base: "./src/content/einbindung"}),
+});
+const feedback = defineCollection({
+  loader: glob({ pattern: ["**/*.mdx", "**/*.md"], base: "./src/content/feedback"}),
+});
 
-export const collections = { kompra1 };
+export const collections = { kompra1, organisieren, einbindung, feedback };
